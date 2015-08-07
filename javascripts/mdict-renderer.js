3deed32963c0b9ff01f51198eeeac1b9a7ab1a8b
Feng Dihai <fengdh@gmail.com>
2015-08-07 09:45:57 +0900

    Add support for mp3 audio

--- javascripts/mdict-renderer.js
+++ javascripts/mdict-renderer.js
@@ -1,205 +1,145 @@
-/*
- * A basic html renderer companioned with mdict-render.js to retrieve word's definition from a MDict dictionary.
- * By Feng Dihai <fengdh@gmail.com>, 2015/07/01
- * 
- * For my wife, my kids and family to whom I'm in love with life.
- *
- * This is free software released under terms of the MIT License.
- * You can get a copy on http://opensource.org/licenses/MIT.
- *
- *
- * MDict software and its file format is developed by Rayman Zhang(张文伟),
- * read more on http://www.mdict.cn/ or http://www.octopus-studio.com/.
- */
+define('mdict-parseXml', function() {
+  return function (str) {
+        return (new DOMParser()).parseFromString(str, 'text/xml');
+    }
+});
 
-/**
- * Usage:
- *   var fileList = ...; // FileList object
- *   var word = ...;     // word for lookup
- *   require(['mdict-parser', 'mdict-renderer'], function(MParser, MRenderer) {
- *      MParser(fileList).then(function(resources) {
- *         var mdict = MRenderer(resources),
- *             dict_desc = resources.description.mdx;
- *         mdict.lookup(word).then(function($content) {
- *            // use $content to display result
- *         });
- *      });
- *    });
- */
-(function (root, factory) {
-  "use strict";
-
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['jquery', 'bluebird', 'speex', 'pcmdata', 'bitstring'], factory);
-  } else {
-    // Browser globals
-    factory(jQuery, Promise);
-  }
-
-}(this, function ($, Promise, SpeexLib, PCMDataLib) {
-  var MIME = {
-    'wav': 'audio/wav',
-    'css': 'text/css',
-    'img': 'image',
-    'spx': 'audio/x-speex',
-    'jpg': 'image/jpeg',
-    'png': 'image/png'
-  };
+require(['jquery', 'mdict-parser', 'mdict-renderer', 'selectize'], function($, MParser, MRenderer, Selectize) {
   
-  function getExtension(filename, defaultExt) {
-    return /(?:\.([^.]+))?$/.exec(filename)[1] || defaultExt;
+  $('#word').selectize({maxItems: 1});
+  
+  var $input = $('#dictfile').on('change', accept);
+  var REGEXP_STRIPKEY = /[., '/\\@_-]()/g;
+
+  function adaptKey(key) { 
+    return key.toLowerCase().replace(REGEXP_STRIPKEY, ''); 
   }
   
-  // TODO: revoke unused resource, LRU
-  // TODO: support for word variation
-  return function createRenderer(resources) {
+  function accept(e) {
+    var fileList = $(e.target).prop('files');
 
-    var cache = (function createCache(mdd) {
-    var repo = {};
-    
-      function get(id, load) {
-        var entry = repo[id];
-        if (!entry) {
-          repo[id] = entry = new Promise(function(resolve) {
-          var will = mdd.then(function(lookup) {
-            console.log('lookup: ' + id);
-            return lookup(id);
-          }).then(load)
-            .then(function(url) { resolve(url); });
-          });
-        }
-        return entry;
-      }
-      
-      return {get: get};
-    })(resources['mdd']);
-    
-    function loadData(mime, data) {
-      var blob = new Blob([data], {type: mime});
-      return URL.createObjectURL(blob);      
-    }
-  
-    function loadAudio(ext, data) {
-      if (ext === 'wav') {
-        return loadData(MIME[ext], data);
-      } else {  // 'spx'
-        var blob = decodeSpeex(String.fromCharCode.apply(null, data));
-        return URL.createObjectURL(blob);
-      }
-    }
-    
-    // TODO: LRU cache: remove oldest one only after rendering.
-    function replaceImage(index, img) {
-      var $img = $(img);
-      var src = $img.attr('src'), m = /^file:\/\/(.*)/.exec(src);
-      if (m) { src = m[1]; }
-      cache.get(src, loadData.bind(null, MIME['img']))
-           .then(function(url) {
-              $img.attr({src: url, src_: src});
+    $('#btnLookup').attr('disabled', true);
+
+    if (fileList.length > 0) {
+        $('#btnLookup').addClass('stripes');
+        $('#word').on('keyup', function(e) { e.which === 13 && $('#btnLookup').click(); });
+
+        MParser(fileList).then(function(resources) {
+          var mdict = MRenderer(resources);
+          
+          function doSearch(phrase, offset) {
+              console.log(phrase + '');
+              mdict.lookup(phrase, offset).then(function($content) {
+                $('#definition').empty().append($content.contents());
+                console.log('--');
+              });
+          }
+          
+          
+          $('#dict-title').html((resources['mdx'] || resources['mdd']).value().description || '** no description **');
+          mdict.render($('#dict-title'));
+          
+          $('#btnLookup')
+            .attr('disabled', false)
+            .off('.#mdict')
+            .on('click.#mdict', function() {
+              doSearch($('#word').val());
+            }).click();
+          
+            $('#word')[0].selectize.destroy();
+          
+            $('#word').selectize({
+              plugins: ['restore_on_backspace'],
+              maxItems: 1,
+              maxOptions: 1 << 20,
+              valueField: 'value',
+              labelField: 'word',
+              searchField: 'word',
+              delimiter: '~~',
+              loadThrottle: 10,
+              create: function(v, callback) {
+                return callback({word: v, key: adaptKey(v), value: v});
+              },
+              createOnBlur: true,
+              closeAfterSelect: true,
+              allowEmptyOption: true,
+              score: function(search) {
+						var score = this.getScoreFunction(search);
+						return function(item) {
+							return 1;
+						};
+					},
+              load: function(query, callback) {
+                var self = this;
+                if (!query.length) {
+                  this.clearOptions();
+                  this.refreshOptions();
+                  return;
+                };
+                
+                mdict.search({phrase: query, forKeys: true, maxCount: 256}).then(function(list) {
+//                  console.log(list.join(', '));
+                  // TODO: filter candidate keyword starting with "_"
+                  list = list.map(function(v) {
+                    return {word: v, key: adaptKey(v), value: v.offset};
+                  });
+                  self.clearOptions();
+                  callback(list);
+                });
+              },
+              onChange: function(value) {
+                var item = this.options[value];
+                if (item) {
+                  var value = item.word;
+                  doSearch(value, value.offset);
+                  $('#word').val(value);
+                } else {
+                  $('#definition').empty();
+                }
+              },
             });
-    }
-    
-    function playAudio(e, $a) {
-      ($a || $(this)).find('audio')[0].play();
-    }
-    
-    function renderAudio() {
-      var $a = $(this);
-      if ($a.attr('href_')) {
-        playAudio($a);
-      } else {
-        var href = $a.attr('href'), res = href.substring(8);
-        var ext = getExtension(res, 'wav');
-        cache.get(res, loadAudio.bind(null, ext))
-             .then(function(url) {
-                $a.append($('<audio>').attr({src: url, src_: href})).on('click', playAudio);
-                setTimeout(playAudio.bind($a));
-             });
-      }
-    }
-    
-    function replaceCss(index, link) {
-      var $link = $(link);
-      var href = $link.attr('href');
-      cache.get(href, loadData.bind(null, MIME['css']))
-           .then(function(url) {
-              // $link.attr({href: url, href_: href});
-              // TODO: Limit scope of embedded styles provide by mdd file
-              // TODO: use shadow dom for Chrome
-              // TODO: use scoped style for Firefox
-              $link.replaceWith($('<style scoped>', {src_: href}).text('@import url("' + url + '")'));
-            });
-    }
-    
-    function decodeSpeex(file) {
-      var ogg = new Ogg(file, {file: true});
-      ogg.demux();
-
-      var header = Speex.parseHeader(ogg.frames[0]);
-      console.log(header);
-
-      var comment = new SpeexComment(ogg.frames[1]);
-      console.log(comment.data);
-
-      var spx = new Speex({
-        quality: 8,
-        mode: header.mode,
-        rate: header.rate
-      });
-      
-      var waveData = PCMData.encode({
-          sampleRate: header.rate,
-          channelCount: header.nb_channels,
-          bytesPerSample: 2,
-          data: spx.decode(ogg.bitstream(), ogg.segments)
         });
-
-      return new Blob([Speex.util.str2ab(waveData)], {type: "audio/wav"});
+    } else {
+      $('#btnLookup').attr('disabled', false);
     }
+    
+    // jump to word with link started with "entry://"
+    // TODO: have to ignore in-page jump
+    $('#definition').on('click', 'a', function(e) {
+      var href = $(this).attr('href');
+      if (href && href.substring(0, 8) === 'entry://') {
+        var word = href.substring(8);
+        // TODO: remove '#' to get jump target
+        if (word.charAt(0) !== '#') {
+          word = word.replace(/(^[/\\])|([/]$)/, '');
 
-    function render($content) {
-      if (resources['mdd']) {
-        $content.find('img[src]').each(replaceImage);
-        
-        $content.find('link[rel=stylesheet]').each(replaceCss);
-        
-        $content.find('a[href^="sound://"]').on('click', renderAudio);
-      }
-      
-      // rewrite in-page link
-      $content.find('a[href^="entry://"]').each(function() { 
-        var $el = $(this), href = $el.attr('href');
-        if (href.match('#')) {
-          $el.attr('href', href.substring(8));
+          $('#word').val(word);
+          $('#btnLookup').click();
         }
-      });
-      
-      return $content;
-    }    
-    
-    return {
-      lookup: function lookup(query) {
-        return (resources['mdx'] || resources['mdd'])
-          .then(function (lookup) {
-            return lookup(query);
-          }).then(function (definitions) {
-            console.log('lookup done!');
-            var html = definitions.reduce(function(prev, txt) { 
-              return prev + '<p></p>' + txt;
-            }, '<p>' + definitions.length + ' entry(ies) </p>');
-            return Promise.resolve(render($('<div>').html(html)));
-          });
-      },
-      
-      search: function (query) {
-        return resources['mdx'].then(function(lookup) {
-          return lookup(query);
-        });
-      },
-      
-      render: render,
-    };
+      }
+    });
   }
 
-}));
+
+});
+
+  
+  
+    /**
+     * Save array buffer data as a download file. 
+     */
+    var saveData = (function() {
+      return function(data, fileName, type) {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        var blob = new Blob([data], { type: type || "octet/stream" });
+        a.href = window.URL.createObjectURL(blob);
+        a.download = fileName;
+        a.click();
+        setTimeout(function() {
+          window.URL.revokeObjectURL(a.href);
+          document.body.removeChild(a);
+        }, 500);
+      };
+    }());
